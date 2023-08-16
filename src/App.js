79b7b2b4b211d77b042a62307ab3036b1ebd433a
pushkar gautam <pushkar.gautam1994@gmail.com>
2023-08-16 21:00:37 +0545

    Linters fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Calculator from './components/calculator.js';
+import Calculator from './components/calculator';
 
 function App() {
   return (
--- src/components/calButtons.js
+++ src/components/calButtons.js
@@ -1,29 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const calculatorButtons = () => {
-  return (
-    <div className="button-container">
-      <button type="button" className="operator">AC</button>
-      <button type="button" className="operator">+/-</button>
-      <button type="button" className="operator">%</button>
-      <button type="button" className="operator">/</button>
-      <button type="button" className="number">7</button>
-      <button type="button" className="number">8</button>
-      <button type="button" className="number">9</button>
-      <button type="button" className="operator">x</button>
-      <button type="button" className="number">4</button>
-      <button type="button" className="number">5</button>
-      <button type="button" className="number">6</button>
-      <button type="button" className="operator">-</button>
-      <button type="button" className="number">1</button>
-      <button type="button" className="number">2</button>
-      <button type="button" className="number">3</button>
-      <button type="button" className="operator">+</button>
-      <button type="button" className="number zero">0</button>
-      <button type="button" className="number">.</button>
-      <button type="button" className="operator equal">=</button>
-    </div>
-  )
-}
+const calculatorButtons = () => (
+  <div className="button-container">
+    <button type="button" className="operator">AC</button>
+    <button type="button" className="operator">+/-</button>
+    <button type="button" className="operator">%</button>
+    <button type="button" className="operator">/</button>
+    <button type="button" className="number">7</button>
+    <button type="button" className="number">8</button>
+    <button type="button" className="number">9</button>
+    <button type="button" className="operator">x</button>
+    <button type="button" className="number">4</button>
+    <button type="button" className="number">5</button>
+    <button type="button" className="number">6</button>
+    <button type="button" className="operator">-</button>
+    <button type="button" className="number">1</button>
+    <button type="button" className="number">2</button>
+    <button type="button" className="number">3</button>
+    <button type="button" className="operator">+</button>
+    <button type="button" className="number zero">0</button>
+    <button type="button" className="number">.</button>
+    <button type="button" className="operator equal">=</button>
+  </div>
+);
 
-export default calculatorButtons
+export default calculatorButtons;
